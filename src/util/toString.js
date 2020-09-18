7cf4b8d3f968c13a168cb2af15cd6d8d6dbbf1e1
linyuanze <1577932922@qq.com>
2020-09-18 12:23:09 +0800

    雷达图展示超长优化

--- src/util/toString.js
+++ src/util/toString.js
@@ -1,4 +1,7 @@
 export default function toString(obj) {
+
+  let formatObj = formatLongName(obj)
+
   let result = JSON.stringify(obj, function(key, val) {
         if (typeof val === 'function') {
             return `~--demo--~${val}~--demo--~`;
@@ -9,5 +12,36 @@ export default function toString(obj) {
     do {
         result = result.replace('\"~--demo--~', '').replace('~--demo--~\"', '').replace(/\\n/g, '').replace(/\\\"/g,"\"");//最后一个replace将release模式中莫名生成的\"转换成"
     } while (result.indexOf('~--demo--~') >= 0);
+
+    result = result.replaceAll('**n**', `\\n`)
+
     return result;
+}
+
+const formatLongName = (obj) => {
+  if (obj.radar && obj.radar.indicator) {
+    obj.radar.indicator.forEach((item, index)=>{
+      if (index === 1 || index === 4) {
+        item.text = splitString(item.text, 6)
+      }
+      if (index === 2 || index === 3) {
+        item.text = splitString(item.text, 9)
+      }
+    })
+  } else {
+    return obj
+  }
+}
+
+const splitString =  (value, splitNum) => {
+  let list = value.split('');
+  let result = '';
+  for (let i = 1; i <= list.length; i++) {
+    if (!(i % splitNum) && list[i] !== undefined) {
+      result += list[i - 1] + `**n**`;
+    } else {
+      result += list[i - 1];
+    }
+  }
+  return result;
 }
